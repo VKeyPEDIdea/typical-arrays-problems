7260cad2ea826756bf4a726bf6b75524096fd470
Karyapkin Pavel <karyapkin@gmail.com>
2020-10-01 21:17:12 +0600

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array.length == 0) return 0;
+  
+  let min = 0;
+  array.forEach(number => {
+    if (number < min) min = number;
+  });
+  
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || array.length == 0) return 0;
+  
+  let max = 0;
+  array.forEach(number => {
+    if (number > max) max = number;
+  });
+  
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array.length == 0) return 0;
+
+  let sum = array.reduce((acc, num) => {
+    acc += num;
+    return acc;
+  });
+
+  return sum / array.length;
 }
